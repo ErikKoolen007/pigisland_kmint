b61bfecbb1fd61bed4f7c30cf6951cd0000bb0de
Erik Koolen <erik-koolen@online.nl>
2018-12-26 15:08:34 +0100

    added a star algorithm

---
+++ pigisland/include/kmint/pigisland/a_star.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "kmint/map/map.hpp"
+#include <queue>
+#include <map>
+
+class a_star
+{
+	kmint::map::map_graph& graph_;
+
+public:
+	a_star(kmint::map::map_graph& graph) : graph_(graph)
+	{
+	}
+
+	std::queue<const kmint::map::map_node*> a_star_search(
+		const kmint::map::map_node& start, const kmint::map::map_node& goal) const;
+
+	std::queue<const kmint::map::map_node*> reconstruct_path(
+		const kmint::map::map_node* start, const kmint::map::map_node* goal,
+		std::map<const kmint::map::map_node*, const kmint::map::map_node*> came_from) const;
+
+	double heuristic(const kmint::map::map_node& a, const kmint::map::map_node& b) const;
+};
